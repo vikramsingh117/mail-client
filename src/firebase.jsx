ecfed3983a444d0e7fb6f90f5d7bebaca4eef49a
Vikram <88143885+vikramsingh117@users.noreply.github.com>
2025-02-16 16:52:06 +0530

    Update firebase.jsx
    removed apis

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // Replace with your Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC5LAmWV9D-8rwZCACpkc3inlXM30-daZE",
-    authDomain: "email-system-2cf39.firebaseapp.com",
-    projectId: "email-system-2cf39",
-    storageBucket: "email-system-2cf39.firebasestorage.app",
-    messagingSenderId: "433646376681",
-    appId: "1:433646376681:web:f3682f86fede11b0f34278",
-    measurementId: "G-0G6PK70NVH"
+    apiKey: 
+    authDomain:
+    projectId: 
+    storageBucket:
+    messagingSenderId:
+    appId: 
+    measurementId: 
   };
   
 const app = initializeApp(firebaseConfig);
